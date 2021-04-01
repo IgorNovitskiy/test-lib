c48116edb5597f4435c472fbf59b2bf021a6d09d
Igor Novitsky <ionovitsky@gmail.com>
2021-04-01 16:24:37 +0300

    Multiple exports

---
+++ src/exampleDiv.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ExampleDiv() {
+  return <div>Example div</div>;
+}
+
+export default ExampleDiv;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import React from 'react'
-import styles from './styles.module.css'
-
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+export { exampleDiv } from './exampleDiv';
+export { exampleP } from './exampleP';
+export { exampleFunction } from './exampleFunction';
